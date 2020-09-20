8b3b548e74b23a622fad74be53863b18b24f2d5a
sendo07 <loakesh.i@gmail.com>
2020-09-20 21:18:42 +0530

    Add: Greeting Schema under models folder

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const servicesRoute = require("./routes/greetings");
+const greetingsRoute = require("./routes/greetings");
 const homepageRoute = require("./routes/homepage");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 // Middleware
 app.use(bodyParser.json());
 app.use("/", homepageRoute);
-app.use("/greetings", servicesRoute);
+app.use("/greetings", greetingsRoute);
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((error) => {
+    console.error("Could not Connect to MongoDB...", error);
+    process.exit();
+  });
 
 /**
  * Using a port if defined in the system env else using 3000
